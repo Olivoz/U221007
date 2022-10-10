39b5e35283dfa5402b0fd867d73d4ef1e8ef9198
Olivoz <lucas.sivars@gmail.com>
2022-10-10 10:32:18 +0200

    Add contacts route

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const port = 5500;
+const contacts = readData();
 
 app.use(express.static("public"));
+
+function readData() {
+  let data = fs.readFileSync("data.json").toString();
+  return JSON.parse(data);
+}
+
+app.get("/contacts", (req, res) => {
+  res.send(contacts);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
