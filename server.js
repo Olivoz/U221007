a4b8bfa1aaf5d030ed227e9d4483fde495b69b56
Olivoz <lucas.sivars@gmail.com>
2022-10-10 11:36:35 +0200

    Implement post request

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const port = 5500;
 const contacts = readData();
 
 app.use(express.static("public"));
+app.use(express.json());
 
 function readData() {
   let data = fs.readFileSync("data.json").toString();
@@ -14,6 +15,17 @@ function readData() {
 app.get("/contacts", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.send(contacts);
+});
+
+app.post("/contacts", (req, res) => {
+  if (req.body.name && req.body.phone) {
+    contacts.contacts.push({ name: req.body.name, phone: req.body.phone });
+    res.status(200);
+    res.end();
+  } else {
+    res.status(400);
+    res.send("Request must be a valid json with name and phone.");
+  }
 });
 
 app.listen(port, () => {
